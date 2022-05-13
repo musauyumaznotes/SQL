3742aa7cb8c1ab8c65d3e91e8827816e36e8d2b1
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2022-05-13 15:53:14 +0300

    13/05/2022
    Kodlar Güzelce Gruplandı Ve Düzenlendi

---
+++ Murat Yücedağ/PostgreSql/PostgreSQLEntityOrnek/PostgreSQLEntityOrnek/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostgreSQLEntityOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Context c = new Context();
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            
+            var degerler = c.Birims.OrderBy(b=>b.birimid).ToList();
+            dataGridView1.DataSource = degerler;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Birim b = new Birim();
+            b.birimad = txtBirimAdi.Text;
+            c.Birims.Add(b);
+            c.SaveChanges();
+            MessageBox.Show("Birim Eklendi");
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtBirimId.Text);
+            var x = c.Birims.Find(id);
+            c.Birims.Remove(x);
+            c.SaveChanges();
+            MessageBox.Show("Birim Başarılı Şekilde Silindi");
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtBirimId.Text);
+            var x = c.Birims.Find(id);
+            x.birimad = txtBirimAdi.Text;
+            c.SaveChanges();
+            MessageBox.Show("Birim Başarılı Bir Şekilde Güncellendi");
+        }
+    }
+}
---
+++ Murat Yücedağ/PostgreSql/PostgreUrunProje/PostgreUrunProje/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace PostgreUrunProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblKategoriId = new System.Windows.Forms.Label();
+            this.txtKategoriId = new System.Windows.Forms.TextBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.txtKategoriAd = new System.Windows.Forms.TextBox();
+            this.lblKategoriAd = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 322);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblKategoriId
+            // 
+            this.lblKategoriId.AutoSize = true;
+            this.lblKategoriId.ForeColor = System.Drawing.Color.White;
+            this.lblKategoriId.Location = new System.Drawing.Point(598, 26);
+            this.lblKategoriId.Name = "lblKategoriId";
+            this.lblKategoriId.Size = new System.Drawing.Size(106, 24);
+            this.lblKategoriId.TabIndex = 1;
+            this.lblKategoriId.Text = "Kategori ID:";
+            // 
+            // txtKategoriId
+            // 
+            this.txtKategoriId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.txtKategoriId.ForeColor = System.Drawing.Color.White;
+            this.txtKategoriId.Location = new System.Drawing.Point(723, 26);
+            this.txtKategoriId.Name = "txtKategoriId";
+            this.txtKategoriId.Size = new System.Drawing.Size(229, 28);
+            this.txtKategoriId.TabIndex = 2;
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(177)))), ((int)(((byte)(140)))));
+            this.btnListele.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.ForeColor = System.Drawing.Color.White;
+            this.btnListele.Location = new System.Drawing.Point(723, 129);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(229, 39);
+            this.btnListele.TabIndex = 3;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // txtKategoriAd
+            // 
+            this.txtKategoriAd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.txtKategoriAd.ForeColor = System.Drawing.Color.White;
+            this.txtKategoriAd.Location = new System.Drawing.Point(723, 76);
+            this.txtKategoriAd.Name = "txtKategoriAd";
+            this.txtKategoriAd.Size = new System.Drawing.Size(229, 28);
+            this.txtKategoriAd.TabIndex = 5;
+            // 
+            // lblKategoriAd
+            // 
+            this.lblKategoriAd.AutoSize = true;
+            this.lblKategoriAd.ForeColor = System.Drawing.Color.White;
+            this.lblKategoriAd.Location = new System.Drawing.Point(598, 76);
+            this.lblKategoriAd.Name = "lblKategoriAd";
+            this.lblKategoriAd.Size = new System.Drawing.Size(119, 24);
+            this.lblKategoriAd.TabIndex = 4;
+            this.lblKategoriAd.Text = "Kategori ADI:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(177)))), ((int)(((byte)(140)))));
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.Color.White;
+            this.btnEkle.Location = new System.Drawing.Point(723, 174);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(229, 39);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(177)))), ((int)(((byte)(140)))));
+            this.btnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.ForeColor = System.Drawing.Color.White;
+            this.btnSil.Location = new System.Drawing.Point(723, 219);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(229, 39);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(177)))), ((int)(((byte)(140)))));
+            this.btnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.ForeColor = System.Drawing.Color.White;
+            this.btnGuncelle.Location = new System.Drawing.Point(723, 264);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(229, 39);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(177)))), ((int)(((byte)(140)))));
+            this.btnAra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAra.ForeColor = System.Drawing.Color.White;
+            this.btnAra.Location = new System.Drawing.Point(723, 309);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(229, 39);
+            this.btnAra.TabIndex = 9;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(962, 370);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtKategoriAd);
+            this.Controls.Add(this.lblKategoriAd);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.txtKategoriId);
+            this.Controls.Add(this.lblKategoriId);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblKategoriId;
+        private System.Windows.Forms.TextBox txtKategoriId;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.TextBox txtKategoriAd;
+        private System.Windows.Forms.Label lblKategoriAd;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
+
